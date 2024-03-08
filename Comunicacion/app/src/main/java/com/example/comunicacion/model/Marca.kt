d63766dab847151360648985f01e5d9e90909876
pauss12 <paulamendezsv@gmail.com>
2024-03-08 11:25:02 +0100

    Clase 08/03/24 en clase

---
+++ Comunicacion/app/src/main/java/com/example/comunicacion/model/Marca.kt
@@ -0,0 +1,11 @@
+package com.example.comunicacion.model
+
+
+//R es la clase que guarda todo
+class Marca (var nombre:String, var imagen: Int, var calidad: String){
+
+    override fun toString(): String {
+
+        return nombre
+    }
+}
