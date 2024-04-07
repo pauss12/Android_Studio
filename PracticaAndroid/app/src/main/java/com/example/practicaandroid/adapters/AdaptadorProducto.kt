021f8d62dc403f8a95b70441d4c1f83c0cb3bf1f
pauss12 <paulamendezsv@gmail.com>
2024-04-07 14:15:15 +0200

    poner un escuchador para poder redirigir la informacion a la otra pestaña

--- PracticaAndroid/app/src/main/java/com/example/practicaandroid/adapters/AdaptadorProducto.kt
+++ PracticaAndroid/app/src/main/java/com/example/practicaandroid/adapters/AdaptadorProducto.kt
@@ -1,6 +1,7 @@
 package com.example.practicaandroid.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.practicaandroid.R
 import com.example.practicaandroid.model.Producto
+import com.example.practicaandroid.ui.DetallesProductoActivity
 
 
 class AdaptadorProducto(var context: Context) :  RecyclerView.Adapter<AdaptadorProducto.MyHolder>() {
@@ -43,6 +45,12 @@ class AdaptadorProducto(var context: Context) :  RecyclerView.Adapter<AdaptadorP
 
         Glide.with(context).load(elemento.image).into(holder.imagen)
 
+        //Controlar que pasa si le das a un elemento
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, DetallesProductoActivity::class.java)
+            intent.putExtra("producto", elemento)
+            context.startActivity(intent)
+        }
     }
 
     fun addProducto(x: Producto)
