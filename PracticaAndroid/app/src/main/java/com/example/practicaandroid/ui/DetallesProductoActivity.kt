075e160ccb97e1289ed9e772a530407e3d1ed542
pauss12 <paulamendezsv@gmail.com>
2024-04-07 18:34:15 +0200

    ya he hecho el rating mas grande y negrita

--- PracticaAndroid/app/src/main/java/com/example/practicaandroid/ui/DetallesProductoActivity.kt
+++ PracticaAndroid/app/src/main/java/com/example/practicaandroid/ui/DetallesProductoActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -16,23 +17,41 @@ class DetallesProductoActivity : AppCompatActivity() {
     private var i: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         binding = ActivityDetallesProductoBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         val producto = intent.getSerializableExtra("producto") as Producto
 
+        //COGER EL NOMBRE DE LA RECETA EN ESPECIFICO -------------------------------
         binding.titulo.text = producto.name
 
+        //CARGAR EL RATING DE ESTA RECETA ------------------
+        val rating = producto.rating.toString()
+        val ratingLista = SpannableStringBuilder("THIS RECIPE HAS A $rating / 5 ")
+        ratingLista.setSpan(StyleSpan(Typeface.BOLD), 17, 26, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        ratingLista.setSpan(RelativeSizeSpan(1.2f), 17, 26, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.rating.text = ratingLista
+
+        //MOSTRAR LA LISTA DE INGREDIENTES --------------------------------------------------
         val ingredientList = SpannableStringBuilder("Ingredientes:\n\n")
+
+        //Poner el titulo de "Ingredientes" en negrita
         ingredientList.setSpan(StyleSpan(Typeface.BOLD), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         for (ingredient in producto.ingredients) {
-            ingredientList.append(" • $ingredient\n")  // Add bullet point and newline
+            ingredientList.append(" • $ingredient\n")
         }
 
         binding.textoIngredientes.text = ingredientList
 
+        //MOSTRAR LA LISTA DE PASOS DE LAS INSTRUCCIONES  --------------------------------------
         val listaPasos = SpannableStringBuilder("Pasos:\n\n")
+
+        //Poner el titulo de "Pasos" en negrita
         listaPasos.setSpan(StyleSpan(Typeface.BOLD), 0, 7  , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         for (paso in producto.instructions) {
             listaPasos.append(" $i. $paso\n\n")
@@ -41,6 +60,7 @@ class DetallesProductoActivity : AppCompatActivity() {
 
         binding.textoPasos.text = listaPasos
 
+        //CARGA LA IMAGEN -----------------------------------------------------------
         Glide.with(this).load(producto.image).into(binding.imagen)
 
     }
