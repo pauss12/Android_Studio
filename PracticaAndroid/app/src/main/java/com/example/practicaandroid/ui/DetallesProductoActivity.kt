53bc2ad7a99e316efc1c721f2a6632bb289d6e66
pauss12 <paulamendezsv@gmail.com>
2024-04-07 17:42:58 +0200

    poner los titulos en negrita

--- PracticaAndroid/app/src/main/java/com/example/practicaandroid/ui/DetallesProductoActivity.kt
+++ PracticaAndroid/app/src/main/java/com/example/practicaandroid/ui/DetallesProductoActivity.kt
@@ -1,6 +1,10 @@
 package com.example.practicaandroid.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.practicaandroid.databinding.ActivityDetallesProductoBinding
@@ -9,6 +13,7 @@ import com.example.practicaandroid.model.Producto
 class DetallesProductoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetallesProductoBinding
+    private var i: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,21 +24,22 @@ class DetallesProductoActivity : AppCompatActivity() {
 
         binding.titulo.text = producto.name
 
-        val ingredientList = StringBuilder()
-        ingredientList.append("Ingredientes: \n\n")
+        val ingredientList = SpannableStringBuilder("Ingredientes:\n\n")
+        ingredientList.setSpan(StyleSpan(Typeface.BOLD), 0, 14, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         for (ingredient in producto.ingredients) {
             ingredientList.append(" • $ingredient\n")  // Add bullet point and newline
         }
 
-        binding.textoIngredientes.text = ingredientList.toString()
+        binding.textoIngredientes.text = ingredientList
 
-        val listaPasos = StringBuilder()
-        listaPasos.append("Pasos: \n\n")
+        val listaPasos = SpannableStringBuilder("Pasos:\n\n")
+        listaPasos.setSpan(StyleSpan(Typeface.BOLD), 0, 7  , Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         for (paso in producto.instructions) {
-            listaPasos.append(" • $paso\n")
+            listaPasos.append(" $i. $paso\n\n")
+            i++;
         }
 
-        binding.textoPasos.text = listaPasos.toString()
+        binding.textoPasos.text = listaPasos
 
         Glide.with(this).load(producto.image).into(binding.imagen)
 
