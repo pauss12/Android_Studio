51fc4025445e78ea44381a73786efcfbf7018c1b
pauss12 <paulamendezsv@gmail.com>
2024-04-07 14:14:47 +0200

    Añadir el "serializable"

--- PracticaAndroid/app/src/main/java/com/example/practicaandroid/model/Producto.kt
+++ PracticaAndroid/app/src/main/java/com/example/practicaandroid/model/Producto.kt
@@ -1,11 +1,13 @@
 package com.example.practicaandroid.model
 
+import java.io.Serializable
+
 class Producto(var id: Int,
                var name: String,
                var ingredients: List<String>,
                var instructions: List<String>,
                var image: String,
-               var rating: Double)
+               var rating: Double) : Serializable
 {
 
 }
