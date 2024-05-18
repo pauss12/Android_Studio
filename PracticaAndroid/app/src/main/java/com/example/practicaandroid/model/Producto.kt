c45fd53e0332cc8f7cba60403666cdc6a82b75a9
pauss12 <paulamendezsv@gmail.com>
2024-05-19 01:28:45 +0200

    he cambiado la clase de "Producto"

--- PracticaAndroid/app/src/main/java/com/example/practicaandroid/model/Producto.kt
+++ PracticaAndroid/app/src/main/java/com/example/practicaandroid/model/Producto.kt
@@ -2,12 +2,13 @@ package com.example.practicaandroid.model
 
 import java.io.Serializable
 
-class Producto(var id: Int,
-               var name: String,
-               var ingredients: List<String>,
-               var instructions: List<String>,
-               var image: String,
-               var rating: Double) : Serializable
-{
+class Producto(
+        var id: Int,
+        var name: String,
+        var ingredients: List<String>?=null,
+        var instructions: List<String>?=null,
+        var image: String?=null,
+        var rating: Double?=null,
+) : Serializable {
 
 }
